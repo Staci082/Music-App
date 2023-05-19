dd25869d20c5eb8dd3cc942a61b8ba0362d91030
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-05-19 18:32:48 +0200

    next button working + song info html added

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 
+// OBJECT PROPERTY VARIABLES
+const songTitle = document.getElementById("song-title")
+const songArtist = document.getElementById("song-artist")
+const songImage = document.getElementById("song-image")
+const songDuration = document.getElementById("time-total")
+
+// BUTTON VARIABLES
+const playlistButton = document.getElementById('playlist-button');
+const searchButton = document.getElementById('search-button');
+const playPauseButton = document.getElementById('playPauseButton');
+const previousButton = document.getElementById('previous-button');
+const nextButton = document.getElementById('next-button');
 
 
 class Spotify {
@@ -11,17 +23,15 @@ class Spotify {
     }
   
 // ADD SONG
-  addSong(title, artist, genre, album, lang, duration, year) {
+  addSong(title, artist, genre, duration, image) {
     let obj = {
       title,
       artist,
       genre,
-      album,
-      lang,
       duration,
-      year,
+      image 
     };
-   return this.songs.push(obj)
+    return this.songs.push(obj)
   }
 
  //  findSongByAnything(property, userInput) {
@@ -41,7 +51,6 @@ class Spotify {
       let song = this.songs.find(song=>song.title === songTitle)
       this.playlist.push(song)
     }
-
 
 // SEARCH BUTTON
     search() {
@@ -64,30 +73,49 @@ class Spotify {
       this.isPlaying = true;
     }
 
+// ADD SONG INFO TO HTML
+    addSongInfo() {
+      songTitle.innerHTML = `${this.songs[this.currentSong].title.toUpperCase()}`
+      songArtist.innerHTML = `${this.songs[this.currentSong].artist.toUpperCase()}`
+      songImage.src = `${this.songs[this.currentSong].image}`
+      songDuration.innerHTML = `${this.songs[this.currentSong].duration}`
+    }
+
 // PREVIOUS SONG BUTTON
     previousSong() {
       console.log("previous")
+      this.addSongInfo()
+
+      let SizeOfSongs = this.songs.length-1
+
+      if(SizeOfSongs === this.currentSong){
+          this.currentSong =0;
+      }else{
+          this.currentSong--
+      }
     }
 
 // NEXT SONG BUTTON
     nextSong() {
       console.log("next")
-    }
+      this.addSongInfo()
+      
+      let SizeOfSongs = this.songs.length-1
 
-
+      if(SizeOfSongs === this.currentSong){
+          this.currentSong =0;
+      }else{
+          this.currentSong++
+      }
+  }
 }
+
 
 // DONT FORGET THIS 
 let app = new Spotify();
 
 
 // BUTTONS
-
-const playlistButton = document.getElementById('playlist-button');
-const searchButton = document.getElementById('search-button');
-const playPauseButton = document.getElementById('playPauseButton');
-const previousButton = document.getElementById('previous-button');
-const nextButton = document.getElementById('next-button');
 
 playPauseButton.addEventListener('click', () => {
   app.isPlaying ? app.pause() : app.play();
@@ -96,28 +124,27 @@ playPauseButton.addEventListener('click', () => {
 playlistButton.addEventListener('click', () => {
   app.displayPlaylist()
 });
+
 searchButton.addEventListener('click', () => {
   app.search()
 });
+
 previousButton.addEventListener('click', () => {
     app.previousSong()
 });
+
 nextButton.addEventListener('click', () => {
     app.nextSong()
 });
 
 
-  
-  
 
-  
-  app.addSong('Toxicity','SOAD','Rock','Toxicity','EN','10','2004');
-  app.addSong('Byob','SOAD','Rock','Unknown','EN','7','2005');
-  app.addSong('My name is','Eminem','Rap','Unknown','EN','5','2005');
-  app.addSong('Before I die','2pac','Rap','Unknown','EN','2','2005');
-  app.addSong('Welcome to the Internet','Bo Burnham','Rap','Unknown','EN','12','2005');
-  app.addSong('Upper Cuts','Terror Reid','Rap','Unknown','EN','3','2005');
-  app.addSong('Upper Cuts1','Terror Reid','Rap','Unknown','EN','3','2005');
+app.addSong('Toxicity','SOAD','Rock', '3.57','./images/music swirl-blue.png');
+app.addSong('Byob','SOAD','Rock','4.29','./images/music swirl-orange.png');
+app.addSong('My name is','Eminem','Rap','3.46','./images/music swirl-purple.png');
+app.addSong('Before I die','2pac','Rap','4.51','./images/music swirl-orange.png');
+app.addSong('Welcome to the Internet','Bo Burnham','Rap','5.24','./images/music swirl-purple.png');
+app.addSong('Upper Cuts','Terror Reid','Rap','4.27','./images/music swirl-blue.png');
 
 
 /* WORKING FUNCTIONS */
