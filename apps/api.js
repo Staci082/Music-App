280e1662dc5691cdfa94082f0f9de9706cc372df
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-06-05 18:09:10 +0200

    retrieving titles from search working

--- apps/api.js
+++ apps/api.js
@@ -5,24 +5,33 @@ const img = document.querySelector("#song-image-container");
 const songTitle = document.querySelector("#song-title");
 const songArtist = document.querySelector("#song-artist");
 
+let searchResponses = [];
+
 const apiUrl = "https://api.deezer.com";
 const apiKey = "9d59965122msh0180ded4becf47bp1d5351jsn3e5337020745"
 
 async function fetchURL(method, callBack) {
   fetch(apiUrl + `/${method}${callBack}` + `&rapidapi-key=${apiKey}`)
-  .then(res=>res.json())
+  .then(response=>response.json())
   .then(data=>{
-    console.log(data)
+      console.log(data)
+      data.data.forEach(title => console.log(title.title))
 })}
+
+
 
 function search(searchInput) { 
   fetchURL("search?q=", searchInput)
+  searchResponses.push(this.target)
 }
 
 function searchGenre(genre) {
   fetchURL("genre/", genre)
-  img.innerHTML = `${data.picture}`
 }
 
-// search("halo")
-searchGenre("rock")
+
+
+  search("in this moment")
+// searchGenre("rock")
+
+console.log(searchResponses)
