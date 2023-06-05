216e3c38cf25ccc61e6110e1c779379842e45e06
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-06-05 15:41:04 +0200

    genre added + search

--- api.js
+++ api.js
@@ -1,20 +1,28 @@
 const searchInput = document.querySelector("#search-input");
 const searchBtn = document.querySelector("#search-button");
+const img = document.querySelector("#song-image-container");
 
-const apiUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
+const songTitle = document.querySelector("#song-title");
+const songArtist = document.querySelector("#song-artist");
+
+const apiUrl = "https://api.deezer.com";
 const apiKey = "9d59965122msh0180ded4becf47bp1d5351jsn3e5337020745"
 
-// q=${searchInput}
-
-async function search(searchInput) { 
-  fetch(apiUrl + searchInput + `&rapidapi-key=${apiKey}`)
+async function fetchURL(method, callBack) {
+  fetch(apiUrl + `/${method}${callBack}` + `&rapidapi-key=${apiKey}`)
   .then(res=>res.json())
   .then(data=>{
-      console.log(data)
-    // data.news.forEach(news=>{
-       //  console.log(news.image)
-    })
+    console.log(data)
+})}
+
+function search(searchInput) { 
+  fetchURL("search?q=", searchInput)
 }
 
-search("halo")
+function searchGenre(genre) {
+  fetchURL("genre/", genre)
+  img.innerHTML = `${data.picture}`
+}
 
+// search("halo")
+searchGenre("rock")
